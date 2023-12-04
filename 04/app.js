abe540af4b2d5b8da004f165e71ac9117b857957
Artur Matysiak <inz.matys@gmail.com>
2023-12-04 13:50:50 +0100

    task done

--- 04/app.js
+++ 04/app.js
@@ -1,9 +1,17 @@
 const user = {
-    firstName: 'Adam',
-    lastName: 'Nowak',
-    born: {
-        day: '14',
-        month: '04',
-        year: '1985'
-    }
-}
+  firstName: "Adam",
+  lastName: "Nowak",
+  born: {
+    day: "4",
+    month: "12",
+    year: "2023",
+  },
+};
+
+checkBirthday = () => {
+  const myDate = new Date(user.born.year, user.born.month - 1, user.born.day).toLocaleString().substring(0, 9);
+  const today = new Date().toLocaleString().substring(0, 9);
+  if (myDate == today) console.log("Happy birthday !!!");
+};
+
+checkBirthday();
